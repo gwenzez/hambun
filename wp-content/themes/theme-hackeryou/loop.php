95553ce35e232187d96a1c66731d7db38af84575
Gwen Zezulka <gwenzez@gmail.com>
2015-03-04 19:48:35 -0500

    styled loop

--- wp-content/themes/theme-hackeryou/loop.php
+++ wp-content/themes/theme-hackeryou/loop.php
@@ -12,35 +12,42 @@
 
 <?php endif; // end if there are no posts ?>
 
+
 <?php // if there are posts, Start the Loop. ?>
 
 <?php while ( have_posts() ) : the_post(); ?>
 
-		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-			<h2 class="entry-title">
-        <a href="<?php the_permalink(); ?>" title="Permalink to: <?php esc_attr(the_title_attribute()); ?>" rel="bookmark">
-          <?php the_title(); ?>
-        </a>
-      </h2>
+	
+	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+		<h2 class="entry-title">
+       		<a href="<?php the_permalink(); ?>" title="Permalink to: <?php esc_attr(the_title_attribute()); ?>" re="bookmark"><?php the_title(); ?></a>
+      	</h2>
+      	<h3><img src="<?php bloginfo( 'template_directory' ); ?>/img/iconmonstr-time-icon.svg"><?php echo get_the_date(); ?> / <img src="<?php bloginfo( 'template_directory' ); ?>/img/iconmonstr-folder-2-icon.svg"> <?php the_tags(' ', ', ', '<br>'); ?></h3>
+		<section class="entry-content">
+			<?php the_content('Continue reading <span class="meta-nav">&rarr;</span>'); ?>
+			<?php wp_link_pages( array(
+         			'before' => '<div class="page-link"> Pages:',
+         			'after' => '</div>'
+       		)); ?>
+		</section><!-- .entry-content -->
+		<footer>
+		<?php
+		$args = array(
+			'post_id' => 1, // use post_id, not post_ID
+		        'count' => true //return only the count
+		);
+		$comments = get_comments($args);
+		echo $comments
 
-			<section class="entry-content">
-				<?php the_content('Continue reading <span class="meta-nav">&rarr;</span>'); ?>
-				<?php wp_link_pages( array(
-          'before' => '<div class="page-link"> Pages:',
-          'after' => '</div>'
-        )); ?>
-			</section><!-- .entry-content -->
+		?>
+			
+       <p><?php comments_popup_link('Respond to this post &raquo;', '1 Response &raquo;', '% Responses &raquo;'); ?><p>
+       <p><?php edit_post_link( 'Edit', '<span class="edit-link">', '</span>' ); ?></p>
+		</footer>
+	</article><!-- #post-## -->
 
-			<footer>
-				<p><?php the_tags('Tags: ', ', ', '<br>'); ?> Posted in <?php the_category(', '); ?></p>
-        <p><?php comments_popup_link('Respond to this post &raquo;', '1 Response &raquo;', '% Responses &raquo;'); ?></p>
-        <p><?php edit_post_link( 'Edit', '<span class="edit-link">', '</span>' ); ?></p>
-			</footer>
-
-		</article><!-- #post-## -->
-
-		<?php comments_template( '', true ); ?>
-
+	<?php comments_template( '', true ); ?>
+	<div class="line"></div>
 
 <?php endwhile; // End the loop. Whew. ?>
 
